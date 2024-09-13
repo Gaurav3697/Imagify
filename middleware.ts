2265bc84c1975c0096cd35c22245fb957969c8dd
Gaurav3697 <gm4063420@gmail.com>
2024-09-13 09:40:17 +0545

    updated route and middleware in hope of fixing the problem

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,7 @@
-import { clerkMiddleware, createRouteMatcher  } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
 // Make sure that the `/api/webhooks/(.*)` route is not protected here
-const isProtectedRoute = createRouteMatcher([
-  '/api/webhooks/(.*)',
-])  
-export default clerkMiddleware((auth,req)=>{
-  if(isProtectedRoute(req))auth().protect()
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
