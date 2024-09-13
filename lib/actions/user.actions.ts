5a5c44a7792c412abb9fa3671b813a1b6e58c8e6
Gaurav3697 <gm4063420@gmail.com>
2024-09-13 09:24:32 +0545

    trying to add data in database from clerk

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -11,6 +11,7 @@ export async function createUser(user: CreateUserParams) {
     await connectToDatabase();
 
     const newUser = await User.create(user);
+    console.log(newUser);
 
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
